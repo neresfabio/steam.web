c0ecc1106acb5f89da033d3fac9e88a42fd4e02a
Neres <neresfjcomunic@gmail.com>
2026-01-20 16:58:16 -0300

    add solução do desafio do dia 20

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,10 +1,28 @@
 import database from "infra/database.js";
 
+// Desafio: Retorne no JSON efaça os testes
+/**
+ * 1) Versão do Postgres
+ * 2) Conexões máximas
+ * 3) Consexões usadas
+ */
+
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
+  const result = await database.query("SHOW server_version;");
+  const versionPost = result.rows[0].server_version;
+  const maxConnectionsResult = await database.query("SHOW max_connections;");
+  const maxConnections = maxConnectionsResult.rows[0].max_connections;
+  const statActivityResult = await database.query(
+    "SELECT count(*) FROM pg_stat_activity WHERE state = 'active';",
+  );
+  const statActivity = statActivityResult.rows[0].count;
+
+  const updatedAt = new Date().toISOString();
   response.status(200).json({
-    chave: "Funcionando ação!",
+    updated_at: updatedAt,
+    version_db: versionPost,
+    max_connections: maxConnections,
+    status_activity: statActivity,
   });
 }
 
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,30 @@
 test("GET to /api/v1/status should return 200", async () => {
+  // Capturando o retorno da API
   const response = await fetch("http://localhost:3000/api/v1/status");
+
+  // Status HTTP
   expect(response.status).toBe(200);
+  const responseBody = await response.json();
+
+  // Estrutura basica
+  // BeDefined: Testa de o valor existe.
+  expect(responseBody.updated_at).toBeDefined();
+  expect(responseBody.version_db).toBeDefined();
+
+  //HaveProperty: Testa se a propriedade existe no Objeto, ou seja, não funciona em strings. Está validando a estrutura da resposta.
+  expect(responseBody).toHaveProperty("version_db");
+  expect(responseBody).toHaveProperty("max_connections");
+  expect(responseBody).toHaveProperty("status_activity");
+
+  // Match: Testa o conteudo, garante que realmente veio uma versão do Postgres, não um texto qualquer.
+  expect(responseBody.version_db).toMatch(/^\d+(\.\d+)?$/);
+
+  // Updated_at deve ser uma ISO Date válida
+  const parsedUpdatedAt = new Date(responseBody.updated_at);
+  expect(parsedUpdatedAt.toISOString()).toBe(responseBody.updated_at);
+
+  // Tipos esperados
+  expect(typeof responseBody.version_db).toBe("string");
+  expect(typeof responseBody.max_connections).toBe("string");
+  expect(typeof responseBody.status_activity).toBe("string");
 });
